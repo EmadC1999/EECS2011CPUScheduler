f55b397cd50eb53a9eaa577b69439c0227bfb93d
leongli <79017154+leongli@users.noreply.github.com>
2021-03-21 21:29:18 -0400

    Create FCFS.java
    FCFS CPU scheduling algorithm

---
+++ src/FCFS.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class FCFS extends base_Scheduling_Class{
+
+
+	public FCFS(ArrayList<process>processes, int numP)
+	{
+		//sort processes by arrival times
+		mergeSortProcesses(processes,numP);
+		numProcesses=numP;
+		//init
+		int turnaroundTime=0,arrivalTime=0; //temp variables
+		for (int i=0; i<numP; i++) //i< number of processes
+		{
+			if (i<numP-1) { // get wait Time
+				// wait time is after first and before last
+				arrivalTime+=processes.get(i).getBurstTime(); 
+				totalWaitTime+=arrivalTime;
+			}
+			// turnaround time is sum of last burst+ and wait time
+			turnaroundTime+=processes.get(i).getBurstTime(); // sum of burst
+			
+			totalTurnAroundTime+=turnaroundTime;//sum of last burst + current process burst
+		}	
+	}
+}
